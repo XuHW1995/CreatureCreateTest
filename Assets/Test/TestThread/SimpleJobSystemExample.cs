dba7973a4d6ddc61df05f448aaf539a2798f6f62
Mr.X <32384543+XuHW1995@users.noreply.github.com>
2024-03-28 19:07:40 +0800

    animaiton rig and jobs test

---
+++ Assets/Test/TestThread/SimpleJobSystemExample.cs
@@ -0,0 +1,53 @@
+﻿using Unity.Burst;
+using UnityEngine;  
+using UnityEngine.Jobs;  
+using UnityEngine.Profiling;  
+using Unity.Collections;  
+using Unity.Jobs;  
+  
+public class SimpleJobSystemExample : MonoBehaviour  
+{  
+    private NativeArray<float> inputArray;  
+    private NativeArray<float> outputArray;  
+  
+    void Start()  
+    {  
+        int length = 1024;  
+        inputArray = new NativeArray<float>(length, Allocator.TempJob);  
+        outputArray = new NativeArray<float>(length, Allocator.TempJob);  
+  
+        // 填充 inputArray 的数据...  
+  
+        var jobHandle = new MyJob(inputArray, outputArray).Schedule(default);  
+        
+        // 等待 Job 完成  
+        jobHandle.Complete();  
+  
+        // 使用 outputArray 的数据...  
+  
+        // 释放 NativeArray 的内存  
+        inputArray.Dispose();  
+        outputArray.Dispose();  
+    }  
+  
+    [BurstCompile]  
+    public struct MyJob : IJob 
+    {  
+        public NativeArray<float> input;  
+        public NativeArray<float> output;
+        
+        public MyJob(NativeArray<float> input, NativeArray<float> output)
+        {
+            this.input = input;
+            this.output = output;
+        }
+        
+        public void Execute()  
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = output[i] * 2;
+            }
+        }  
+    }  
+}
---
+++ Assets/Test/TestThread/TestThread.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SharedData {  
+    public int count = 0;  
+}  
+
+public class TestThread : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SharedData data = new SharedData(); 
+        
+        Thread a = new Thread((o =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                data.count++; // 可能导致数据竞争  
+            }
+        }));  
+        
+        Thread b = new Thread((o =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                data.count++; // 可能导致数据竞争  
+            }
+        }));  
+        
+        a.Start();  
+        b.Start();
+
+        a.Join(); 
+        b.Join();  
+
+        Debug.Log($"XHW Data count = {data.count}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
